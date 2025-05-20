c9fa6daf064f91bc1c97c7e8630691784a8c594a
jayh969 <jlh969@uowmail.edu.au>
2025-05-20 13:16:41 +1000

    prior to cleaning

--- src/Visualisation-of-sequence.py
+++ src/Visualisation-of-sequence.py
@@ -20,21 +20,21 @@ for x in file_list:
     sequence = pd.read_csv(f'{input_path}{x}')
     sequence['graph_negatives'] = - sequence ['final_nucleotide_identity']
     sequences.append(sequence)
-# codons = pd.read_csv('results/Optimised-sequences_ENV.csv')
 
 
-def plot_identity():
-    fig, ax = plt.subplots( figsize=(25, 20))
-    plt.stem(range(len(codons)), codons['nucleotide_identity'])
-    plt.stem(range(len(codons)), codons['graph_negatives'])
-    plt.xlabel('Position')
-    ax.set_yticks(range(-3,4))
-    ax.set_yticklabels((3, 2, 1, 0, 1, 2, 3))
-    return fig 
 
-plot_identity()
+# def plot_identity():
+#     fig, ax = plt.subplots( figsize=(25, 20))
+#     plt.stem(range(len(codons)), codons['nucleotide_identity'])
+#     plt.stem(range(len(codons)), codons['graph_negatives'])
+#     plt.xlabel('Position')
+#     ax.set_yticks(range(-3,4))
+#     ax.set_yticklabels((3, 2, 1, 0, 1, 2, 3))
+#     return fig 
 
-plt.savefig('ENV.svg')
+# plot_identity()
+
+# plt.savefig('ENV.svg')
 
 # Compare codon usage to optimal frequency
 # -> add optimal frequencies to the codon-table.csv as a new column DONE
@@ -70,9 +70,6 @@ codons['aminoacid'] = codons['final_codon'].map(amino_dic)
     # group together codon and amino acid
     # count function via groupby
 
-
-# final_codons.sort_values(['aminoacid'])
-
 proportion = (codons[['aminoacid', 'final_codon', 'final_nucleotide_identity']].groupby(['aminoacid', 'final_codon']).count() / codons[['aminoacid', 'final_nucleotide_identity']].groupby(['aminoacid']).count()).reset_index()
 
 codon_frac_dic = codon_map.set_index('Codon')['Fraction'].to_dict()
@@ -103,57 +100,8 @@ def plot_freq():
 
 plot_freq()
 
-#   # Optional: if the segment is small or 0, customize the labels
-# labels = [v.get_height() if v.get_height() > 0 else '' for v in c]
-    
-#     # remove the labels parameter if it's not needed for customized labels
-#     ax.bar_label(c, labels=labels, label_type='center'
-#identifying UG repeats
-
-GTG_repeats = codons[codons['final_codon'] == 'GTG'].copy()
-TGT_repeats = codons[codons['final_codon'] == 'TGT'].copy()
-
-
-cluster_TGT = []
-for row in TGT_repeats.index:
-    cluster_TGT.append(row)
-
-cluster_GTG = []
-for row in GTG_repeats.index:
-    cluster_GTG.append(row)
-
-from itertools import groupby
-from operator import itemgetter
-
-
-GTG_cluster = []
-for k, g in groupby(enumerate(cluster_GTG), lambda ix : ix[0] - ix[1]):
-    GTG_cluster.append((list(map(itemgetter(1), g))))
-
-
-
-
-TGT_cluster = []
-for k, g in groupby(enumerate(cluster_TGT), lambda ix : ix[0] - ix[1]):
-    TGT_cluster.append((list(map(itemgetter(1), g))))
-
-
-
-
-for x in TGT_cluster:
-  GTG_cluster.append(x)
-
-total_cluster = GTG_cluster
-
-
-
-clusters_3 = [x for x in total_cluster if len(x)>=3]
-
-flattened_clusters = [item for sublist in clusters_3 for item in sublist]
-
-
-
-codons['GTGT_clustered?']= [1 if x in flattened_clusters else 0 for x in codons.index.tolist()]
+# identifying UG repeats
+# Identify TG repeats and single As
 
 
 proportion.to_csv(f'{output_folder}comparison-opt-ENV.csv')
@@ -189,14 +137,120 @@ codons['final_codon_GTGT_free'] = final_codons
 
 
 
+# Visualise nucleotide identity
+
+file_list = [filename for filename in os.listdir(input_path) if 'Optimised-sequences_' in filename]
+
+sequences = []
+for x in file_list:
+    sequence = pd.read_csv(f'{input_path}{x}')
+    sequence['graph_negatives'] = - sequence ['final_nucleotide_identity']
+    sequence['data_source'] = x
+    sequences.append(sequence)
+sequences = pd.concat(sequences)
+
+sequences['protein'] = sequences['data_source'].str.split('_').str[-1].str.split('.').str[0]
+
+graph_pos = {'ENV':10,'POL':20,'PRO':30,'GAG':40}
+
+sequences['adjusted_position_ro'] = sequences['protein'].map(graph_pos) + sequences['graph_negatives']
+
+sequences['adjusted_position_o'] = sequences['protein'].map(graph_pos) + sequences['nucleotide_identity']
 
 
+#Locate TG repeats in final codon
+
+GTG_repeats = sequences[sequences['final_codon'] == 'GTG'].copy()
+TGT_repeats = sequences[sequences['final_codon'] == 'TGT'].copy()
 
 
+cluster_TGT = []
+for row in TGT_repeats.index:
+    cluster_TGT.append(row)
+
+cluster_GTG = []
+for row in GTG_repeats.index:
+    cluster_GTG.append(row)
+
+from itertools import groupby
+from operator import itemgetter
+
+GTG_cluster = []
+for k, g in groupby(enumerate(cluster_GTG), lambda ix : ix[0] - ix[1]):
+    GTG_cluster.append((list(map(itemgetter(1), g))))
+
+TGT_cluster = []
+for k, g in groupby(enumerate(cluster_TGT), lambda ix : ix[0] - ix[1]):
+    TGT_cluster.append((list(map(itemgetter(1), g))))
+
+for x in TGT_cluster:
+  GTG_cluster.append(x)
+
+total_cluster = GTG_cluster
+
+clusters_3 = [x for x in total_cluster if len(x)>=3]
+
+flattened_clusters = [item for sublist in clusters_3 for item in sublist]
+
+sequences['GTGT_clustered?']= [1 if x in flattened_clusters else 0 for x in sequences.index.tolist()]
 
 
+#TG repeats new_codon
+GTG_repeats = sequences[sequences['new_codon'] == 'GTG'].copy()
+TGT_repeats = sequences[sequences['new_codon'] == 'TGT'].copy()
+
+cluster_TGT = []
+for row in TGT_repeats.index:
+    cluster_TGT.append(row)
+
+cluster_GTG = []
+for row in GTG_repeats.index:
+    cluster_GTG.append(row)
+
+from itertools import groupby
+from operator import itemgetter
+
+GTG_cluster = []
+for k, g in groupby(enumerate(cluster_GTG), lambda ix : ix[0] - ix[1]):
+    GTG_cluster.append((list(map(itemgetter(1), g))))
+
+TGT_cluster = []
+for k, g in groupby(enumerate(cluster_TGT), lambda ix : ix[0] - ix[1]):
+    TGT_cluster.append((list(map(itemgetter(1), g))))
+
+for x in TGT_cluster:
+  GTG_cluster.append(x)
+
+total_cluster = GTG_cluster
+
+clusters_3 = [x for x in total_cluster if len(x)>=3]
+
+flattened_clusters = [item for sublist in clusters_3 for item in sublist]
+
+sequences['GTGT_clustered?_new_codon']= [1 if x in flattened_clusters else 0 for x in sequences.index.tolist()]
+
+#Graphing
+
+tick_labels = dict(sequences[['adjusted_position_ro', 'graph_negatives']].values)
+tick_labels.update(sequences[['adjusted_position_o', 'nucleotide_identity']].values)
+
+y_labels = ('ENV','POL','PRO','GAG')
+
+col = np.where(sequences['GTGT_clustered?']==1,'k',np.where(sequences['GTGT_clustered?_new_codon'], 'r'))
 
 
+fig, ax = plt.subplots( figsize=(35, 25))
+for protein, df in sequences.groupby('protein'):
+    plt.stem(range(len(df)), df['adjusted_position_ro'], bottom=graph_pos[protein])
+    plt.stem(range(len(df)), df['adjusted_position_o'], bottom=graph_pos[protein])
+plt.xlabel('Position',fontsize=30)
+ax.set_yticks(list(tick_labels.keys()),labels=list(tick_labels.values()), fontsize=30)
+ax.set_ylim(5,45)
+ax.set_xticks(ax.get_xticks(), labels = [int(x) for x in ax.get_xticks()], fontsize=30)
+ax.set_xlim(0,1000)
+
+
+plt.savefig('ENV.svg')
 
 
 
